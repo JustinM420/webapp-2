51aefe05b121e5bebc941ab8fcba449a3cbc6f6d
Justinm420 <125757215+JustinM420@users.noreply.github.com>
2023-02-20 16:33:31 +0000

    added signup/login and removed tutorial gets

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
 from flask import Flask, render_template, jsonify
 import openai
-import numpy as np  # standard math module for python
-from pprint import pprint
+import os
+import numpy as np
 
-def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
-
-
-openai.api_key = open_file('openaiapikey.txt')
-
-
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['Mycos:', 'USER:']):
-    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.Completion.create(
-        engine=engine,
-        prompt=prompt,
-        temperature=temp,
-        max_tokens=tokens,
-        top_p=top_p,
-        frequency_penalty=freq_pen,
-        presence_penalty=pres_pen,
-        stop=stop)
-    text = response['choices'][0]['text'].strip()
-    return text
-
-
-if __name__ == '__main__':
-    conversation = list()
-    while True:
-        user_input = input('USER: ')
-        conversation.append('USER: %s' % user_input)
-        text_block = '\n'.join(conversation)
-        prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
-        prompt = prompt + '\nMycos:'
-        response = gpt3_completion(prompt)
-        print('Mycos:', response)
-        conversation.append('Mycos: %s' % response)
 
 app = Flask (__name__)
 
@@ -75,6 +41,32 @@ def hello_world():
   @app.route("/api/strains")
   def list_strains():
     return jsonify(STRAINS)
+
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+         if request.method == 'POST':
+            # Handle the form submission
+            username = request.form['username']
+            password = request.form['password']
+            # Add the user to your database
+            return redirect('/login')
+            # Render the sign up page
+            return render_template('signup.html')
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            # Handle the login form submission
+            username = request.form['username']
+            password = request.form['password']
+            # Check if the username and password are correct
+            # If they are, log the user in and redirect to the homepage
+            return redirect('/')
+        # Render the login page
+        return render_template('login.html')
+
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
